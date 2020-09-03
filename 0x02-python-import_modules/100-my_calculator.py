28d3ddd35bae86ca9614f4df4c421a16c7f1b402
DanielVillalba-holberton <1821@holbertonschool.com>
2020-09-03 19:32:44 +0000

    code optimized with dictionary

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,25 +1,26 @@
 #!/usr/bin/python3
-from calculator_1 import add, div, mul, sub
 from sys import argv
+from calculator_1 import add, sub, mul, div
 
 
 def main():
-    argc = len(argv) - 1
+    if len(argv) != 4:
+        print('Usage: {} <a> <operator> <b>'.format(argv[0]))
+        exit(1)
 
-    if argv[2] not in ['+', '-', '*', '/']:
-        print("Unknown operator. Available operators: +, -, * and /")
-        exit(1)
-    if argc != 3:
-        print("Usage: {} <a> <operator> <b>".format(argv[0]))
-        exit(1)
-    else:
-        if argv[2] == '+':
-            print("{} + {} = {}".format(argv[1], argv[3], add(int(argv[1]), int(argv[3]))))
-        elif argv[2] == '/':
-            print("{} / {} = {}".format(argv[1], argv[3], div(int(argv[1]), int(argv[3]))))
-        elif argv[2] == '-':
-            print("{} - {} = {}".format(argv[1], argv[3], sub(int(argv[1]), int(argv[3]))))
-        elif argv[2] == '*':
-            print("{} * {} = {}".format(argv[1], argv[3], mul(int(argv[1]), int(argv[3]))))
+    dic_oper = {'+': add, '-': sub, '*': mul, '/': div}
+    a = int(argv[1])
+    b = int(argv[3])
+    sel = argv[2]
+
+    for op in dic_oper:
+        if op == sel:
+            res = dic_oper[op](a, b)
+            print('{:d} {:} {:d} = {:d}'.format(a, op, b, res))
+            exit(0)
+
+    print('Unknown operator. Available operators: +, -, * and /')
+    exit(1)
+
 if __name__ == '__main__':
     main()
