b15ff257f0a7801592e32bd81951838dffd8cc73
DanielVillalba-holberton <1821@holbertonschool.com>
2020-09-11 00:40:19 +0000

    fixed case is empty

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python3
 def weight_average(my_list=[]):
-    sum, elem = 0, 0
-    for i, j in my_list:
-        sum += i * j
-        elem += j
-    return (sum / elem)
+    if my_list:
+        sum, elem = 0, 0
+        for i, j in my_list:
+            sum += i * j
+            elem += j
+        return (sum / elem)
+    else:
+        return 0
