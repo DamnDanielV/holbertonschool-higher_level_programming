44a08093563f9d614ee2e62227e5258be7e44a02
DanielVillalba-holberton <1821@holbertonschool.com>
2020-09-21 16:19:43 +0000

    function that divides 2 integers and prints the result.

---
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+
+def safe_print_division(a, b):
+    try:
+        di = a / b
+    except:
+        di = None
+    finally:
+        print("Inside result: {}".format(di))
+    return di
