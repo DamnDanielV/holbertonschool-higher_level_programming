5c6d7b7b2d94a5c3a288a598c8250ea94a36939e
DanielVillalba-holberton <1821@holbertonschool.com>
2020-09-22 20:38:03 +0000

    fixed

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -2,12 +2,5 @@
 
 
 class Square:
-    """se inicializan los atributos con __init"""
     def __init__(self, size):
-        """tres tipos de atributos:
-        privados: self.__priv
-        protegidos: self._prot
-        publicos: self.publ
-        """
-        """este ejercicio requiere un atributo privado de objeto"""
         self.__size = size
