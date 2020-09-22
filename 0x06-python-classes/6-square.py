df9d9724fbc0264ffeb205bcd9267d3a336571d6
DanielVillalba-holberton <1821@holbertonschool.com>
2020-09-22 22:47:41 +0000

    test to fix(logical order changed)

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -19,12 +19,11 @@ class Square:
         """ prints a square of '#' """
         if (self.__size == 0):
             print()
-        else:
-            for j in range(self.__position[1]):
-                print()
-            for j in range(self.__size):
-                print(" " * self.__position[0], end="")
-                print("#" * self.__size)
+            return
+        for j in range(self.__position[1]):
+            print()
+        for j in range(self.__size):
+            print("{}{}".format(" " * self.position[0], "#" * self.size))
 
     @property
     def size(self):
