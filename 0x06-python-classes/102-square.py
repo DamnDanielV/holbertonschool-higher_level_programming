f4a2337730ec3b902147b5864280b5c7db98c3b1
DanielVillalba-holberton <1821@holbertonschool.com>
2020-09-22 23:47:21 +0000

    compares two squares

---
+++ 0x06-python-classes/102-square.py
@@ -0,0 +1,54 @@
+#!/usr/bin/python3
+""" Square Module """
+
+
+class Square:
+    """ This is a docstring of Square Class """
+    def __init__(self, size=0):
+        """ defining the size of square """
+        if type(size) != int:
+            raise TypeError("size must be an integer")
+        else:
+            if size >= 0:
+                self.__size = size
+            else:
+                raise ValueError("size must be >= 0")
+
+    def area(self):
+        """ area of a square """
+        ar = self.__size ** 2
+        return ar
+
+    @property
+    def size(self):
+        """ size variable of Square class instance """
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """ sets size variable of Square class instance """
+        if type(value) != int:
+            raise TypeError("size must be an integer")
+        else:
+            if value >= 0:
+                self.__size = value
+            else:
+                raise ValueError("size must be >= 0")
+
+    def __eq__(self, other):
+        return self.__size == other.__size
+
+    def __ne__(self, other):
+        return self.__size != other.__size
+
+    def __lt__(self, other):
+        return self.__size < other.__size
+
+    def __le__(self, other):
+        return self.__size <= other.__size
+
+    def __gt__(self, other):
+        return self.__size > other.__size
+
+    def __ge__(self, other):
+        return self.__size >= other.__size
