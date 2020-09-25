ad184f26acae5d1f42a4a2a270a195578f567df8
DanielVillalba-holberton <1821@holbertonschool.com>
2020-09-25 19:10:56 +0000

    added more case test

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -4,16 +4,26 @@
 import unittest
 max_integer = __import__('6-max_integer').max_integer
 
+ma = [2, 3, 4, 5]
+
 
 class TestMaxInteger(unittest.TestCase):
     """class max integer"""
     def test_max_integer(self):
         """# test max in a list"""
-        self.assertEqual(max_integer([2, 3, 4, 5]), 5)
+        self.assertEqual(max_integer(ma), 5)
 
     def test_empty_list(self):
         """test when empty list"""
         self.assertEqual(max_integer([]), None)
+
+    def test_is_list(self):
+        """test to verify if is a list"""
+        assert(isinstance(ma, list))
+
+    def test_list_integers(self):
+        for num in ma:
+            assert(isinstance(num, int))
 
 if __name__ == '__main__':
     unittest.main()
