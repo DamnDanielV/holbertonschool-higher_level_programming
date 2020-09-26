d71654221d46352c16485c5eb1d3c094cd102d0d
DanielVillalba-holberton <1821@holbertonschool.com>
2020-09-26 01:42:46 +0000

    fixed expected failure

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -4,43 +4,50 @@
 import unittest
 max_integer = __import__('6-max_integer').max_integer
 
-ma = [2, 3, 4, 5]
-
 
 class TestMaxInteger(unittest.TestCase):
     """class max integer"""
     def test_max_integer(self):
         """# test max in a list"""
+        ma = [2, 3, 4, 5]
         self.assertEqual(max_integer(ma), 5)
 
     def test_empty_list(self):
         """test when empty list"""
-        self.assertEqual(max_integer([]), None)
+        self.assertIsNone(max_integer([]))
 
     def test_compare_string(self):
         """test case compare strings"""
         self.assertEqual(max_integer(["Daniel", "Villalba"]), "Villalba")
 
-    def test_is_list(self):
-        """test to verify if is a list"""
-        assert(isinstance(ma, list))
+    def test_None_list(self):
+        """Test case None in List"""
+        self.assertIsNone(max_integer([None]))
 
-    def test_list_integers(self):
+    def test_no_arguments(self):
         """test if the item is a int"""
-        for num in ma:
-            assert(isinstance(num, int))
+        self.assertIsNone(max_integer())
 
 
 class TestExpectedFailure(unittest.TestCase):
     """class for expected failures in execution"""
+    @unittest.expectedFailure
+    def test_booleans(self):
+        """to avoid the error of raise"""
+        self.assertEqual(max_integer([False, True]), False)
+
     def test_string_in_list(self):
-        """test case string in a list"""
         with self.assertRaises(TypeError):
-            max_integer(["hello", 3, 4, 5])
+            max_integer(["Daniel", 1, 2, 3])
 
     def test_too_Arguments(self):
         with self.assertRaises(TypeError):
             max_integer([3, 2, 7], [2, 4, 5])
+
+    def test_with_set(self):
+        """test when is a set"""
+        with self.assertRaises(TypeError):
+            max_integer({3, 6, 5, 1})
     # test for cases with None's
 
     def test_case_None(self):
