d010c63462fb1363b7e7bd08e868ea2630d4c92f
DanielVillalba-holberton <1821@holbertonschool.com>
2020-10-07 21:04:19 +0000

    reads n lines of a text file (UTF8) and prints it to stdout

---
+++ 0x0B-python-input_output/2-read_lines.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+
+def read_lines(filename="", nb_lines=0):
+    """read a given number of lines of a text file"""
+    if nb_lines < 0:
+        nb_lines = 0
+    with open(filename, mode="r", encoding="utf-8") as f:
+        for c, line in enumerate(f):
+            if nb_lines == c and nb_lines:
+                break
+            print(line, end="")
+        f.close()
