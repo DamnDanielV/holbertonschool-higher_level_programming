e68362a4eec71a59a65657416944f55da9fede24
DanielVillalba-holberton <1821@holbertonschool.com>
2020-10-07 22:47:33 +0000

    fixed docstring

--- 0x0B-python-input_output/8-load_from_json_file.py
+++ 0x0B-python-input_output/8-load_from_json_file.py
@@ -4,6 +4,7 @@ import json
 
 
 def load_from_json_file(filename):
+    """function module json"""
     with open(filename, mode="r", encoding="utf-8") as f:
         obj = json.loads(f.read())
         f.close()
