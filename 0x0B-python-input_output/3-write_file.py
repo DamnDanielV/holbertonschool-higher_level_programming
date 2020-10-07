bf043f01931ccf82bb9760d9d5180a69d0fe4a4c
DanielVillalba-holberton <1821@holbertonschool.com>
2020-10-07 21:04:50 +0000

    writes a string to a text file

---
+++ 0x0B-python-input_output/3-write_file.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+
+def write_file(filename="", text=""):
+    """writes a file text"""
+    ch_wr = 0
+    with open(filename, mode="w", encoding="utf-8") as f:
+        ch_wr += f.write(text)
+        f.close()
+    return ch_wr
