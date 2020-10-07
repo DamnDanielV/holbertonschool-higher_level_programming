e8781363fcbeeb0d41badd0edd678c0e9c11c62e
DanielVillalba-holberton <1821@holbertonschool.com>
2020-10-07 22:21:04 +0000

    fixed

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-"""documentacion"""
+"""documentacion kaffadf"""
 
 def read_file(filename=""):
     """reads a file"""
     with open(filename, 'r') as f:
-        print(f.readlines(), end="")
+        print(f.read(), end="")
