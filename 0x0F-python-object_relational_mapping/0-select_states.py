fa8c5622197ddeb534a7cc51e6f46cfdf62bbfdb
DamnDanielV <1821@holbertonschool.com>
2020-12-13 15:26:36 -0500

    fixed

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -7,11 +7,12 @@ import MySQLdb
 def list_db():
     """list a db"""
     db = MySQLdb.connect(
-        host='localhost',
+        host="localhost",
         port=3306,
         user=sys.argv[1],
         passwd=sys.argv[2],
-        db=sys.argv[3]
+        db=sys.argv[3],
+        charset="utf8"
     )
     cur = db.cursor()
     cur.execute("SELECT * FROM states ORDER BY states.id ASC")
