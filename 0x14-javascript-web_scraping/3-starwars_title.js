85c77805a48355badc1c07b57ecdc30d4925f287
DamnDanielV <1821@holbertonschool.com>
2021-02-09 21:38:14 -0500

    errores bobooooossss jhadgafgakjfg

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -2,13 +2,13 @@
 const request = require('request');
 
 if (process.argv.length >= 3) {
-  let url = 'https://swapi-api.hbtn.io/api/films/' + process.argv[2];
+  const url = 'https://swapi-api.hbtn.io/api/films/' + process.argv[2];
 
   request(url, (err, status, body) => {
     if (err) {
       console.log(err);
     } else {
-      console.log(JSON.parse(body)['title']);
+      console.log(JSON.parse(body).title);
     }
   });
 }
--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -3,19 +3,19 @@ const request = require('request');
 
 const url = process.argv[2];
 request(url, (err, res, body) => {
-  if (err) { 
-      console.log(err);
-    } else {
+  if (err) {
+    console.log(err);
+  } else {
     let count = 0;
     const movies = JSON.parse(body).results;
     movies.forEach(movie => {
-        let chars = movie['characters'];
-        chars.forEach(id => {
-          if (id.includes('18')) {
-            count += 1;
-          }
-        });
+      const chars = movie.characters;
+      chars.forEach(id => {
+        if (id.includes('18')) {
+          count += 1;
+        }
       });
+    });
     console.log(count);
   }
 });
